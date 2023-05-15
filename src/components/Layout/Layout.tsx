19bb3ea93270e028b9824d40060a26c3136c8c16
Drake Costa <drake@saeris.io>
2023-05-15 00:59:32 -0700

    more ios fixes

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { AnimatePresence } from "framer-motion";
 import { Waves } from "../Waves";
 import styles from "./Layout.module.css";
 
 export const Layout: React.FC<{ children: React.ReactNode }> = ({
   children
-}) => (
-  <>
-    <Waves />
-    <main className={styles.container}>
-      <AnimatePresence
-        initial={false}
-        mode="wait"
-        onExitComplete={(): void => window.scrollTo(0, 0)}
-      >
-        {children}
-      </AnimatePresence>
-    </main>
-  </>
-);
+}) => {
+  const appHeight = (): void => {
+    if (typeof document !== `undefined`) {
+      document.documentElement.style.setProperty(
+        `--app-height`,
+        `${window.innerHeight}px`
+      );
+    }
+  };
+
+  useLayoutEffect(() => {
+    if (typeof window !== `undefined`) {
+      window.addEventListener(`resize`, appHeight);
+    }
+    appHeight();
+
+    return () => {
+      if (typeof window !== `undefined`) {
+        window.removeEventListener(`resize`, appHeight);
+      }
+    };
+  });
+
+  return (
+    <>
+      <Waves />
+      <main className={styles.container}>
+        <AnimatePresence
+          initial={false}
+          mode="wait"
+          onExitComplete={(): void => window.scrollTo(0, 0)}
+        >
+          {children}
+        </AnimatePresence>
+      </main>
+    </>
+  );
+};
