f5f7824037606e8adafd6f7e6525bac9fb8de425
Drake Costa <drake@saeris.io>
2023-05-15 02:01:26 -0700

    ios css is hard af

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,45 +1,21 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { AnimatePresence } from "framer-motion";
 import { Waves } from "../Waves";
 import styles from "./Layout.module.css";
 
 export const Layout: React.FC<{ children: React.ReactNode }> = ({
   children
-}) => {
-  const appHeight = (): void => {
-    if (typeof document !== `undefined`) {
-      document.documentElement.style.setProperty(
-        `--app-height`,
-        `${window.innerHeight}px`
-      );
-    }
-  };
-
-  useEffect(() => {
-    if (typeof window !== `undefined`) {
-      window.addEventListener(`resize`, appHeight);
-    }
-    appHeight();
-
-    return () => {
-      if (typeof window !== `undefined`) {
-        window.removeEventListener(`resize`, appHeight);
-      }
-    };
-  });
-
-  return (
-    <>
-      <Waves />
-      <main className={styles.container}>
-        <AnimatePresence
-          initial={false}
-          mode="wait"
-          onExitComplete={(): void => window.scrollTo(0, 0)}
-        >
-          {children}
-        </AnimatePresence>
-      </main>
-    </>
-  );
-};
+}) => (
+  <>
+    <Waves />
+    <main className={styles.container}>
+      <AnimatePresence
+        initial={false}
+        mode="wait"
+        onExitComplete={(): void => window.scrollTo(0, 0)}
+      >
+        {children}
+      </AnimatePresence>
+    </main>
+  </>
+);
